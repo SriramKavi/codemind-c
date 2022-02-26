4b024d62726309ecb4250fb02207f67993a35e80
SriramKavi <21A91A04L1@aec.edu.in>
2022-02-26 13:48:11 +0530

    #include<stdio.h> int main() { 	int n,r,max=0; 	scanf("%d",&n); 	while(n>0) 	{ 		r=n%10; 		if(max<r) 		{ 			max=r; 		} 		n=n/10; 	} 	printf("%d ",max); 	return 0; }

---
+++ Largest_Digit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,max=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		if(max<r)
+		{
+			max=r;
+		}
+		n=n/10;
+	}
+	printf("%d
+",max);
+	return 0;
+}
